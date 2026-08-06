a30fda6eb2d598c839842091ff57fc470a348a91
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2026-08-06 15:30:15 +0800

    add

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-    base: '/wdd330-classcheck/',
-})
+    base: "/wdd330-classcheck/",
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                attendance: resolve(__dirname, "attendance.html"),
+                student: resolve(__dirname, "student.html"),
+            },
+        },
+    },
+});
